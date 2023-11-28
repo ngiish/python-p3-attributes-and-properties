127ce5188fdcfa3d91b07bae7682b6025672e181
Isaac <govegeku00@gmail.com>
2023-11-28 10:04:34 +0300

    Initial commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,30 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+
+    def __init__(self):
+        self._name = ""
+        self._breed = ""
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <=25:
+            self._name = name
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    name = property(get_name, set_name,)   
+
+    def get_breed(self):
+        print("Getting the breed.")
+        return self._breed
+    def set_breed(self, breed):
+        if (type(breed) in APPROVED_BREEDS):
+            print(f"Setting breed to {breed}")
+            self._breed = breed
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    breed = property (get_breed, set_breed,)
